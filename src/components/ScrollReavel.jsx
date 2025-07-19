c92120385babb0593bf98e49c5333270b9e72adb
sabbir-sabbir <iosabbir7890@gmail.com>
2025-07-19 23:15:57 +0600

    add

--- src/components/ScrollReavel.jsx
+++ src/components/ScrollReavel.jsx
@@ -5,11 +5,11 @@ import { motion } from "framer-motion";
 const Box = ({ text }) => {
   return (
     <motion.div
-    initial={{opacity: 0, x: -50}}
+    initial={{opacity: 0, x: -20}}
     whileInView={{opacity: 1, x: 0, transition: {
         duration: 2
     }}}
-    className="w-[200px] h-[200px] mt-2 ml-2 flex items-center justify-center border-2 bg-orange-400">
+    className="w-[150px] h-[150px] mt-2 ml-2 flex items-center justify-center border-2 bg-orange-400  hover:w-[175px] duration-700 ">
       {text}
     </motion.div>
   );
